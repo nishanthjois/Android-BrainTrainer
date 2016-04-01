a87c7bb0d16b4a04d7e16550b2e6d7d1c5428da0
nishanthjois <nishanth.jois@gmail.com>
2016-04-01 21:33:05 +0530

    Final version of Brain Trainer

--- main/java/com/braintrainer/nj/braintrainer/MainActivity.java
+++ main/java/com/braintrainer/nj/braintrainer/MainActivity.java
@@ -1,16 +1,17 @@
 package com.braintrainer.nj.braintrainer;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,18 +19,67 @@ public class MainActivity extends AppCompatActivity {
     TextView resultTextView;
     TextView scoreTextView;
     TextView sumTextView;
+    TextView timerTextView;
+    TextView lastScore;
+    Button playAgainButton;
     Button button0,button1,button2,button3;
+    RelativeLayout gameRelativeLayout, launchRelativeLayout;
     ArrayList<Integer> answers = new ArrayList<Integer>();
+    ArrayList<Integer> lastFiveScores = new ArrayList<Integer>();
     int locationOfCorrectAnswer;
     int score = 0;
     int numberOfQuestions = 0;
 
+    public void playAgain (View view){
+
+        score=0;
+        numberOfQuestions = 0;
+        timerTextView.setText("30s");
+        scoreTextView.setText("0/0");
+        resultTextView.setText("");
+        playAgainButton.setVisibility(View.INVISIBLE);
+        lastScore.setVisibility(View.INVISIBLE);
+
+        generateQuestion();
+
+        new CountDownTimer(30100,1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerTextView.setText(String.valueOf(millisUntilFinished/1000)+"s");
+
+            }
+
+            @Override
+            public void onFinish() {
+
+
+                timerTextView.setText("0s");
+                resultTextView.setText("Your Score: " + Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
+                startButton.setVisibility(View.VISIBLE);
+                gameRelativeLayout.setVisibility(RelativeLayout.INVISIBLE);
+                launchRelativeLayout.setVisibility(RelativeLayout.VISIBLE);
+                lastScore.setVisibility(View.VISIBLE);
+                lastScore.setText("Your Score: " + Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
+
+               // lastFiveScores.add(score);
+               // playAgainButton.setVisibility(View.VISIBLE);
+
+            }
+        }.start();
+
+
+
+
+    }
+
     public void generateQuestion(){
         Random rand = new Random();
-        int a = rand.nextInt(21); //Random number between 0 and 20
-        int b = rand.nextInt(21);
+        int a = rand.nextInt(99); //Random number between 0 and 20
+        int b = rand.nextInt(99);
+        int c = rand.nextInt(79);
 
-        sumTextView.setText(Integer.toString(a) + "+" + Integer.toString(b));
+        sumTextView.setText(Integer.toString(a) + "+" + Integer.toString(b)+ "-" + Integer.toString(c));
         answers.clear();
         locationOfCorrectAnswer = rand.nextInt(4);
         int inCorrectAnswer;
@@ -37,12 +87,12 @@ public class MainActivity extends AppCompatActivity {
         for (int i=0;i<4;i++){
 
             if (i==locationOfCorrectAnswer){
-                answers.add(a+b);
+                answers.add(a+b-c);
             } else {
-                inCorrectAnswer=rand.nextInt(41);
+                inCorrectAnswer=rand.nextInt(200);
 
                 while (inCorrectAnswer==locationOfCorrectAnswer){
-                    inCorrectAnswer=rand.nextInt(41);
+                    inCorrectAnswer=rand.nextInt(200);
                 }
                 answers.add(inCorrectAnswer);
             }
@@ -73,12 +123,14 @@ public class MainActivity extends AppCompatActivity {
         numberOfQuestions++;
         scoreTextView.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
         generateQuestion();
-        Log.i("Tag", (String)view.getTag());
+        Log.i("Tag", (String) view.getTag());
     }
 
     public void start(View view){
 
         startButton.setVisibility(View.INVISIBLE);
+        gameRelativeLayout.setVisibility(RelativeLayout.VISIBLE);
+      playAgain(findViewById(R.id.startButton));
     }
 
     @Override
@@ -92,21 +144,27 @@ public class MainActivity extends AppCompatActivity {
         button1 = (Button) findViewById(R.id.answerButton1);
         button2 = (Button) findViewById(R.id.answerButton2);
         button3 = (Button) findViewById(R.id.answerButton3);
+        playAgainButton = (Button) findViewById((R.id.playAgainButton));
         resultTextView = (TextView) findViewById(R.id.resultTextView);
         scoreTextView = (TextView) findViewById(R.id.scoreTextView);
+        timerTextView = (TextView) findViewById(R.id.timertextView);
+        lastScore = (TextView) findViewById(R.id.LastScoreTextView);
+        gameRelativeLayout = (RelativeLayout) findViewById(R.id.gameRelativeLayout);
+        launchRelativeLayout = (RelativeLayout) findViewById(R.id.launchRelativeLayout);
 
-        generateQuestion();
+       // lastFiveScores.clear();
+
 
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
-    @Override
+*/
+    /*@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -119,5 +177,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
